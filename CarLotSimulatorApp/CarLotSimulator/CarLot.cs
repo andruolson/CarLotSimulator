fb08718bfd18b4c442bbeebe63271da2d700824e
Andrew Olson <andruolson@gmail.com>
2025-10-21 19:18:38 -0600

    added CarLot class.

--- CarLotSimulatorApp/CarLotSimulator/CarLot.cs
+++ CarLotSimulatorApp/CarLotSimulator/CarLot.cs
@@ -7,11 +7,22 @@ public class CarLot
 {
     public List<Car> ParkingLot { get; set; } = new List<Car>();
 
+    public class Carlot
+    {
+        public static int numberOfCars = 0;
+
+        public Carlot()
+        {
+            numberOfCars++;
+        }
+    }
     public void CheckCars()
     {
         foreach (var vehicle in ParkingLot)
         {
             Console.WriteLine($"{vehicle.Year} {vehicle.Make} {vehicle.Model}.");
         }
+        
+        
     }
 }
